ac6715b17ba05a8e7736b0bf0889c81e206a4e9e
BirushaNdegeya <birushandegeya@gmail.com>
2024-06-28 14:34:38 +0200

    fix prod bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer(): JSX.Element {
    return (
       <footer className="bg-blue-500 text-white p-4 text-center">
          <div className="container mx-auto">
-            <p>&copy; {new Date().getFullYear()} Sofia. All rights reserved.</p>
+            <p>&lsquo; {new Date().getFullYear()} Sofia. All rights reserved.</p>
          </div>
       </footer>
    );
